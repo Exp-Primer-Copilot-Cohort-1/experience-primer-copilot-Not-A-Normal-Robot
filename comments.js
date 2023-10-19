be1c86e9a29204d9e4a5a9c95c3c6a48a0308445
NOT_A_ROBOT <76723917+Not-A-Normal-Robot@users.noreply.github.com>
2023-10-19 13:13:33 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,15 @@
-const http = require('http');
+// Create a basic web server
+// Run it by typing: node comments.js
+// Load http://localhost:3000 in your browser and see the result
+// Stop the server with CTRL+C
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Load the http module to create an http server.
+var http = require('http');
+var comments = require('./comments.json');
 
-const server = http.createServer((req, res) => {
-    console.log(`Received request for ${req.url}`);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.writeHead(200, {"Content-Type": "application/json"});
+  response.end(JSON.stringify(comments));
 });
